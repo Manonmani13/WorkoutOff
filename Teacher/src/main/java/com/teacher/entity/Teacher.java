a827259a60644a0d5545e3a22539f0cc4b7000e2
Manonmani13 <manonmani.pandukumar@gmail.com>
2021-12-08 19:41:52 +0530

    teacher

---
+++ Teacher/src/main/java/com/teacher/entity/Teacher.java
@@ -0,0 +1,39 @@
+package com.teacher.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity(name="TeacherDetails")
+public class Teacher {
+@Id
+private int id;
+private String name;
+private String subject;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getSubject() {
+	return subject;
+}
+public void setSubject(String subject) {
+	this.subject = subject;
+}
+public Teacher(int id, String name, String subject) {
+	super();
+	this.id = id;
+	this.name = name;
+	this.subject = subject;
+}
+public Teacher()
+{
+}
+}
